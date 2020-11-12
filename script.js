2f5048e62103f2aa971a97edf6b4a18ca43b0a24
ttngu <tammyn137@gmail.com>
2020-11-11 19:47:41 -0800

    add API Key, query URL and ajax call

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-// Pseudo Code:
-// Create function - on click for city search
+// Defined Variables
+var APIKey = "104f10c4aa9b29656ab75accd63edf29";
+
+
+// Create function - on click for city name search
+function getWeather(cityName) {
+    
+    // URL to query the Open Weather database, adding our cityName and API Key
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+
+    // AJAX call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    })
+    
+}
     // Present CURRENT and future conidtions
         // city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index 
+
             // UX index presented with color that indicates whether the conditions are favorable, moderate, or severe
+
     // Present FUTURE conditions
         // 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+
     // Add city to recent search log
+
         // Display recent searches under search input
+
         // If clicked again, present with current and future conditions for that city
+
     // Keep last sesarch in local storage
+
         // When page is opened, present the last searched city results.
