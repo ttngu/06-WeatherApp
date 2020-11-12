8a65558809df24722122ed5f569845f09d4c0728
ttngu <tammyn137@gmail.com>
2020-11-11 18:23:38 -0800

    added js pseudo code

---
+++ script.js
@@ -0,0 +1,12 @@
+// Pseudo Code:
+// Create function - on click for city search
+    // Present CURRENT and future conidtions
+        // city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index 
+            // UX index presented with color that indicates whether the conditions are favorable, moderate, or severe
+    // Present FUTURE conditions
+        // 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+    // Add city to recent search log
+        // Display recent searches under search input
+        // If clicked again, present with current and future conditions for that city
+    // Keep last sesarch in local storage
+        // When page is opened, present the last searched city results.
